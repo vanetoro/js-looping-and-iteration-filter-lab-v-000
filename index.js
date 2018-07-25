0a629339d8dcd890340145fab827f1210b05c50b
Vanessa Toro <vanetoro84@gmail.com>
2018-07-25 17:59:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
 function findMatching(driver_name){
+  debugger
   return drivers.filter(function(name){return name.toLowerCase() == driver_name.toLowerCase()})
 }
