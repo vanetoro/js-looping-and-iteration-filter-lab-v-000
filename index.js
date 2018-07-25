e1d3d18d9e2d0a1771d4a42efac2f9a1d6dddcc1
Vanessa Toro <vanetoro84@gmail.com>
2018-07-25 18:02:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
 function findMatching(arr,driver_name){
-  return arr.filter(function(name){return name == driver_name})
+  return arr.filter(function(name){return name.toLowerCase() == driver_name.toLowerCase()})
 }
