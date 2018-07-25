939e7b6c63cc9e871b67febd3b08215ea432e832
Vanessa Toro <vanetoro84@gmail.com>
2018-07-25 17:52:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
 function findMatching(driver_name){
-  return drivers.filter(function(name){return name == driver_name})
+  return drivers.filter(function(name){return name.toUpperCase() == driver_name.toUpperCase()})
 
 }
