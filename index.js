ce85247ea28ecd06ca2ae87d56e413ab24b5a389
Vanessa Toro <vanetoro84@gmail.com>
2018-07-25 18:01:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
 function findMatching(driver_name){
   debugger
-  return drivers.filter(function(name){return name == driver_name.})
+  return drivers.filter(function(name){return name == driver_name})
 }
