f03f1d311178542220e23038434878449d2a60e0
Vanessa Toro <vanetoro84@gmail.com>
2018-07-25 17:58:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solution in this file
 
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
 function findMatching(driver_name){
   return drivers.filter(function(name){return name == driver_name})
 }
