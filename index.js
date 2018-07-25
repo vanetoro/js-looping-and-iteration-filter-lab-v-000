bca7f6efc79a673b306208e54e1e26f76c45cd86
Vanessa Toro <vanetoro84@gmail.com>
2018-07-25 18:09:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 function findMatching(arr,driver_name){
   return arr.filter(function(name){return name.toLowerCase() == driver_name.toLowerCase()})
 }
+
+
+function fuzzyMatch(){
+    return drivers.filter(function(name){return name[0] == driver_name[0]})
+}
